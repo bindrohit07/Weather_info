37e773b4d8894dc63071cc4e2e5495523723ae2b
Rohit Kumar Bind <rohitbind9@gmail.com>
2022-12-03 14:31:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Temp from './Components/Weather'
+import "./App.css"
+import Weather from './Components/Weather'
+const App = () => {
+  return (
+    
+    <>
+    <Weather/>
+    </>
+  )
+}
+
+export default App
